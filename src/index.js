f5e78087403c76ea1bcb66546ffcad996fe58e94
manuelwecker <manuelwecker@web.de>
2019-11-08 11:44:05 +0100

    add function for image implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const cocktailZombie = {
-  pic: "Zombie-Pic",
+  pic:
+    "https://i-love-png.com/images/1516736773745713231free-bloody-mary-clipart.hi.png",
 
   title: "Zombie",
 
@@ -12,15 +13,44 @@ const cocktailZombie = {
   fav: false
 };
 
+const mainTag = document.querySelector("#main");
+
 function createDivClass(element, divClass) {
   element = document.createElement(element);
   element.setAttribute("class", divClass);
   return element;
 }
 
-let test = createDivClass("div", "box");
-test.innerHTML = cocktailZombie.title;
-console.log(test);
+function createDivClassContent(element, divClass, content) {
+  if (element == "img") {
+    element = document.createElement(element);
+    element.setAttribute("src", content);
+    element.setAttribute("class", divClass);
+  } else {
+    element = document.createElement(element);
+    element.innerHTML = content;
+    element.setAttribute("class", divClass);
+  }
+  return element;
+}
+
+let cardBox = createDivClass("div", "box");
+let cardImage = createDivClass("div", "box__image");
+let imageContent = createDivClassContent(
+  "img",
+  "crop-center",
+  cocktailZombie.pic
+);
+let cardContent = createDivClass("div", "box__content");
+let cardFav = createDivClass("div", "box__favorite");
+
+mainTag.appendChild(cardBox);
+cardBox.appendChild(cardImage);
+cardBox.appendChild(cardContent);
+cardBox.appendChild(cardFav);
+cardImage.appendChild(imageContent);
+
+// console.log(test);
 
 // console.log(cocktailZombie.title);
 // const mainTag = document.querySelector("#main");
@@ -29,3 +59,9 @@ console.log(test);
 // const childElement = document.createElement("div");
 // divElement.setAttribute("class", "box");
 // divElement.appendChild(childElement);
+
+function createDivClass(element, divClass, contentArray) {
+  element = document.createElement(element);
+  element.setAttribute("class", divClass);
+  return element;
+}
